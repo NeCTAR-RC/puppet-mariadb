6479ba253629c0dfde15cb2a6d0148a769a29ab9
Sam Morrison <sorrison@gmail.com>
2014-03-18 20:07:11 +1100

    Fix up apt key download
    Change-Id: I2deeb9ad081931ce4767f387e28758c6e1642d37

--- manifests/repository.pp
+++ manifests/repository.pp
@@ -3,7 +3,7 @@ class mariadb::repository {
   if $::rfc1918_gateway == 'true' {
     exec { 'mariadb-apt-key':
      path        => '/usr/bin:/bin:/usr/sbin:/sbin',
-     command     => "apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 1BB943DB --keyserver-options http-proxy=\"${::http_proxy}\"",
+     command     => "apt-key adv --recv-keys --keyserver keyserver.ubuntu.com --keyserver-options http-proxy=\"${::http_proxy}\" 1BB943DB",
      unless      => 'apt-key list | grep 1BB943DB >/dev/null 2>&1',
     }
 
