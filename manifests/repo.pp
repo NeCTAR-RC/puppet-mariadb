4ebbb54afad44924847a09feadac9fa2b343d339
Simon Kelada-Polupanov <simon.kelada@unimelb.edu.au>
2014-05-30 14:26:22 +1000

    Option to use proxy when adding apt key
    Change-Id: I30bcce5d6f8fac6873ea7b1c22459ed1a5a29eae

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,9 +1,18 @@
 class mariadb::repo {
 
   apt::source { 'mariadb':
-    location    => 'http://mirror.aarnet.edu.au/pub/MariaDB/repo/5.5/ubuntu',
-    repos       => 'main',
-    key         => '1BB943DB',
+    location => 'http://mirror.aarnet.edu.au/pub/MariaDB/repo/5.5/ubuntu',
+    repos    => 'main',
   }
 
+  $key_options = $::rfc1918_gateway ? {
+    'true'  => "http-proxy=http://${::http_proxy_server}:${::http_proxy_port}",
+    default => false,
+  }
+
+  apt::key { 'mariadb':
+    key         => '1BB943DB',
+    key_server  => 'pgp.mit.edu',
+    key_options => $key_options,
+  }
 }
