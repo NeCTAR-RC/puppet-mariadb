07daaa769f7ef1d6315fd7d53904338b3527a267
Sam Morrison <sorrison@gmail.com>
2015-03-02 14:05:01 +1100

    Fixup Redhat7 repo url, closes #14
    Change-Id: I3da0c7a179c9a11f4e81c7f28af386c08653ccf4

--- manifests/repo/redhat.pp
+++ manifests/repo/redhat.pp
@@ -2,7 +2,7 @@ class mariadb::repo::redhat {
   $version = $mariadb::repo_version
 
   yumrepo { 'mariadb':
-    baseurl         => "http://yum.mariadb.org/${version}/rhel\$releasever-amd64/",
+    baseurl         => "http://yum.mariadb.org/${version}/rhel${operatingsystemmajrelease}-amd64/",
     enabled         => '1',
     gpgcheck        => '1',
     gpgkey          => 'https://yum.mariadb.org/RPM-GPG-KEY-MariaDB',
