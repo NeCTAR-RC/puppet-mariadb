bd0893b56e6bf31d0abf119ddb640453e92cdb94
Dietrich Heise <d.heise@madsack.de>
2015-06-04 12:16:45 +0200

    fix $galera_ensure variable

--- manifests/cluster.pp
+++ manifests/cluster.pp
@@ -72,7 +72,7 @@ class mariadb::cluster (
   $package_names           = $mariadb::params::cluster_package_names,
   $package_ensure          = $mariadb::params::cluster_package_ensure,
   $galera_name             = $mariadb::params::galera_package_name,
-  $galera_ensure           = $mariadb::params::galera_package_ensure,
+  $galera_ensure           = $mariadb::params::cluster_package_ensure,
   $debiansysmaint_password = undef,
   $status_password         = undef,
   $repo_version            = '5.5',
