8da7ca2756b6802e16e975e5c6399be84d65b37c
Sam Morrison <sorrison@gmail.com>
2016-07-11 11:15:31 +1000

    Fix up deps for wsrep user
    Change-Id: Ib003de73f11024a56bc9027adb688865bc5e1af7

--- manifests/cluster/auth.pp
+++ manifests/cluster/auth.pp
@@ -6,11 +6,12 @@ class mariadb::cluster::auth (
   database_user { "${wsrep_sst_user}@%":
     ensure        => present,
     password_hash => mysql_password($wsrep_sst_password),
+    require       => Class['mariadb::server'],
   }
 
   database_grant { "${wsrep_sst_user}@%":
     privileges => [ 'all' ],
+    require    => Database_user["${wsrep_sst_user}@%"],
   }
 
 }
-
