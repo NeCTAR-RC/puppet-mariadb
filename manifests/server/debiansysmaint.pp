3e6cccbc2f47701a1bb675746b7dcf5df646d6e2
Sam Morrison <sorrison@gmail.com>
2023-03-16 14:19:55 +1100

    Revert "Specifically set permissions for debian user"
    Different versions of mariadb have different perms so
    just set to all to make it easier
    
    This reverts commit eae4c8017e13646c2140c7e44c5e18f1650e996a.
    
    Change-Id: Iff3546dde4622a5f22faea9fff1def72d46a857f

--- manifests/server/debiansysmaint.pp
+++ manifests/server/debiansysmaint.pp
@@ -11,12 +11,7 @@ class mariadb::server::debiansysmaint (
   mysql_grant { 'debian-sys-maint@localhost/*.*':
     user       => 'debian-sys-maint@localhost',
     table      => '*.*',
-    privileges => [ 'ALTER', 'ALTER ROUTINE', 'BINLOG ADMIN', 'BINLOG MONITOR', 'BINLOG REPLAY', 'CONNECTION ADMIN',
-                    'CREATE', 'CREATE ROUTINE', 'CREATE TEMPORARY TABLES', 'CREATE USER', 'CREATE VIEW', 'DELETE',
-                    'DROP', 'EVENT', 'EXECUTE', 'FEDERATED ADMIN', 'FILE', 'INDEX', 'INSERT', 'LOCK TABLES', 'PROCESS',
-                    'READ_ONLY ADMIN', 'REFERENCES', 'RELOAD', 'REPLICATION MASTER ADMIN', 'REPLICATION SLAVE',
-                    'REPLICATION SLAVE ADMIN', 'SELECT', 'SET USER', 'SHOW DATABASES', 'SHOW VIEW', 'SHUTDOWN',
-                    'SLAVE MONITOR', 'SUPER', 'TRIGGER', 'UPDATE'],
+    privileges => [ 'all' ],
     require    => Database_user['debian-sys-maint@localhost'],
   }
 
