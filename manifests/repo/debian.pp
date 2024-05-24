9cae325bc997c3a8389c56754733ee743c0a8bee
Sam Morrison <sorrison@gmail.com>
2024-05-24 13:42:40 +1000

    Remove deprecated http_proxy apt key
    Change-Id: I24cebf9cca10890ff3158a7ca085a2138c32a6c8

--- manifests/repo/debian.pp
+++ manifests/repo/debian.pp
@@ -14,22 +14,13 @@ class mariadb::repo::debian {
     repos    => 'main',
   }
 
-  if defined('$::http_proxy') and str2bool($::rfc1918_gateway) {
-    $key_options = "http-proxy=${::http_proxy}"
-  }
-  else {
-    $key_options = undef
-  }
-
   apt::key { 'mariadb-1':
     id      => '199369E5404BD5FC7D2FE43BCBCB082A1BB943DB',
     server  => 'keyserver.ubuntu.com',
-    options => $key_options,
   }
   apt::key { 'mariadb-2':
     id      => '177F4010FE56CA3336300305F1656F24C74CD1D8',
     server  => 'keyserver.ubuntu.com',
-    options => $key_options,
   }
 
   Apt::Source <| title == 'mariadb' |> -> Class['apt::update'] -> Package <| tag == 'mariadb' |>
